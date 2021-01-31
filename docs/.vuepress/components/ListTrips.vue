18791843b6106f5d4ef7ccac1d84f82af6982b38
Lukas Hirt <lukashirt@mail.com>
2021-01-31 19:18:19 +0100

    Graphql POC

--- docs/.vuepress/components/ListTrips.vue
+++ docs/.vuepress/components/ListTrips.vue
@@ -4,9 +4,35 @@
   </ul>
 </template>
 <script>
+import gql from 'graphql-tag'
+
 export default {
   data: () => ({
-    trips: ["Trip 1", "Trip2", "Trip3"]
-  })
+    trips: []
+  }),
+
+  created() {
+    this.getTrips()
+  },
+
+  methods: {
+    async getTrips() {
+      const query = `
+        query {
+          offers{
+            title
+            location,
+            description,
+            titleImageUrl,
+          }
+        }
+        `
+      const trips = await this.axios.post('http://localhost:9000/query', {
+        query
+      })
+
+      console.log(trips)
+    }
+  }
 }
 </script>
