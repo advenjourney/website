e663f4cd5f4424888c0af3b695e60d16c2458c16
Lukas Hirt <lukashirt@mail.com>
2021-02-03 20:18:24 +0100

    Add trip creation

--- docs/.vuepress/components/ListTrips.vue
+++ docs/.vuepress/components/ListTrips.vue
@@ -1,16 +1,46 @@
 <template>
-  <ul>
-    <li v-for="trip in trips" :key="trip.title" v-text="trip.title" />
-  </ul>
+  <section>
+    <div v-for="(trip, index) in trips" :key="index">
+      <img :src="trip.titleImageUrl" />
+      <h3 v-text="trip.title" />
+      <span v-text="trip.location" />
+      <p v-text="trip.description" />
+    </div>
+    <div v-if="authorized">
+      <h3>Add new trip</h3>
+      <div>
+        <input v-model="title" placeholder="Title" />
+      </div>
+      <div>
+        <input v-model="image" placeholder="Image address" />
+      </div>
+      <div>
+        <input v-model="location" placeholder="Location" />
+      </div>
+      <div>
+        <textarea v-model="description" placeholder="Description" />
+      </div>
+      <button @click="createTrip">Add new trip</button>
+    </div>
+  </section>
 </template>
 <script>
 export default {
   data: () => ({
-    trips: []
+    trips: [],
+    title: null,
+    location: null,
+    image: null,
+    description: null,
+    authorized: false
   }),
 
-  created() {
+  mounted() {
     this.getTrips()
+
+    if (sessionStorage.getItem('token')) {
+      this.authorized = true
+    }
   },
 
   methods: {
@@ -25,9 +55,28 @@ export default {
           }
         }
         `
-      const trips = await this.axios.post('http://localhost:9000/query', { query }, { headers: { 'Content-Type': 'application/json' } })
+      const trips = await this.axios.post('http://localhost:9000/query', { query })
 
       this.trips = trips.data.data.offers
+    },
+
+    async createTrip() {
+      const query = `
+        mutation {
+          createOffer(input: {title: "${this.title}", location: "${this.location}", description: "${this.description}", titleImageUrl: "${this.image}"}){
+            id,
+            title,
+            location,
+            description,
+            titleImageUrl,
+            user{
+              name
+            }
+          }
+        }
+        `
+      const trips = await this.axios.post('http://localhost:9000/query', { query }, { headers: { 'Authorization': 'Bearer ' + sessionStorage.getItem('token')} })
+      console.log(trips)
     }
   }
 }
